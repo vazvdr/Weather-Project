702ffb03302f7b4e3d0ab6710ebffd6b866c6dd1
Vanderson de Azevedo <127865294+vazvdr@users.noreply.github.com>
2024-05-18 21:05:29 -0300

    Subindo a conexao com o banco de dados

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+
+    $server = 'localhost';
+    $usuario = 'root';
+    $senha = '';
+    $banco = 'cadastro-weather';
+
+    $conexao = new mysqli($server, $usuario, $senha, $banco);
+
+?> 
