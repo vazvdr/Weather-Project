65fb7423a67c05ecaefce0ea5ff25b1a5e88fcaf
vazvdr <azevedo.vanderson@hotmail.com>
2024-07-04 19:58:40 -0300

    Enviando os arquivos para fazer deploy

---
+++ scripts.js
@@ -0,0 +1,123 @@
+const apiKey = "6605ccc65869cc9c3ad42a6d0fab22f9";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector("#weather-data");
+
+const errorMessageContainer = document.querySelector("#error-message");
+const loader = document.querySelector("#loader");
+
+const suggestionContainer = document.querySelector("#suggestions");
+const suggestionButtons = document.querySelectorAll("#suggestions button");
+
+// Loader
+const toggleLoader = () => {
+  loader.classList.toggle("hide");
+};
+
+const getWeatherData = async (city) => {
+  toggleLoader();
+
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
+
+  toggleLoader();
+
+  return data;
+};
+
+// Tratamento de erro
+const showErrorMessage = () => {
+  errorMessageContainer.classList.remove("hide");
+};
+
+const hideInformation = () => {
+  errorMessageContainer.classList.add("hide");
+  weatherContainer.classList.add("hide");
+
+  suggestionContainer.classList.add("hide");
+};
+
+const showWeatherData = async (city) => {
+  hideInformation();
+
+  const data = await getWeatherData(city);
+
+  if (data.cod === "404") {
+    showErrorMessage();
+    return;
+  }
+
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+  );
+  countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+  umidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
+
+  //remove as sugestões e mostra as informações
+  weatherContainer.classList.remove("hide");
+};
+
+searchBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const city = cityInput.value;
+
+  showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+  if (e.code === "Enter") {
+    const city = e.target.value;
+
+    showWeatherData(city);
+  }
+});
+
+// Sugestões
+suggestionButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const city = btn.getAttribute("id");
+
+    showWeatherData(city);
+  });
+});
+
+function summerMode(){
+  document.getElementById('btnSummer').className = 'buttonsSummer';
+  document.getElementById('btnCloud').className = 'buttonsSummer';
+  document.getElementById('btnSnow').className = 'buttonsSummer';
+  document.getElementById('container').className = 'containerSummer';
+  document.getElementById('bodyInicial').className = 'bodySummer';
+}
+function snowMode(){
+  document.getElementById('btnSummer').className = 'buttons';
+  document.getElementById('btnSnow').className = 'buttons';
+  document.getElementById('btnCloud').className = 'buttons';
+  document.getElementById('container').className = 'container';
+  document.getElementById('bodyInicial').className = 'bodySnow';
+}
+function CloudMode(){
+  document.getElementById('btnSummer').className = 'buttonsCloud';
+  document.getElementById('btnSnow').className = 'buttonsCloud';
+  document.getElementById('btnCloud').className = 'buttonsCloud';
+  document.getElementById('container').className = 'containerCloud';
+  document.getElementById('bodyInicial').className = 'bodyCloud';
+}
